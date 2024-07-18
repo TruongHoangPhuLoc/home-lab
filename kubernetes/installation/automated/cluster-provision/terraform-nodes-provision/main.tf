95c5f8309b046a842e1a08b7cd1de99af5274cee
TruongHoangPhuLoc <truonghoangphuloc@phus-MacBook-Pro.local>
2024-07-18 14:20:51 +0700

    worker nodes joining

--- kubernetes/installation/automated/cluster-provision/terraform-nodes-provision/main.tf
+++ kubernetes/installation/automated/cluster-provision/terraform-nodes-provision/main.tf
@@ -11,10 +11,8 @@ module "k8s-master-nodes-provision" {
 source = "../../../../../terraform/proxmox-provider/provision-vm"
 proxmox_params = {
   "pm_api_url": "https://172.16.1.253:8006/api2/json", 
-  # Change
-  # pm_user="user"
-  # Change
-  # pm_password="Mypassword"
+  "pm_user": "root@pam", "pm_password": "Phuloc@99.", 
+  "pm_debug": true, 
   "pm_tls_insecure": true 
 }
 target_node = "geekom-dev"
@@ -27,7 +25,7 @@ instance_configruations = {
       amount = 4096
     }
     networking = {
-      ip = "172.16.1.113"
+      ip = "172.16.1.231"
     }
   }
   k8s-master-02 = {
@@ -38,7 +36,7 @@ instance_configruations = {
         amount = 4096
     }
     networking = {
-      ip = "172.16.1.114"
+      ip = "172.16.1.232"
     }
   }
   k8s-master-03 = {
@@ -49,17 +47,50 @@ instance_configruations = {
         amount = 4096
     }
     networking = {
-        ip = "172.16.1.115"
+        ip = "172.16.1.233"
     }
   }
 
 }
-
+}
+module "k8s-worker-nodes-provision" {
+source = "../../../../../terraform/proxmox-provider/provision-vm"
+proxmox_params = {
+  "pm_api_url": "https://172.16.1.253:8006/api2/json", 
+  "pm_user": "root@pam", "pm_password": "Phuloc@99.", 
+  "pm_debug": true, 
+  "pm_tls_insecure": true 
+}
+target_node = "geekom-dev"
+instance_configruations = {
+  k8s-worker-01 = {
+    cpu = {
+      cores = 2
+    }
+    memory = {
+      amount = 4096
+    }
+    networking = {
+      ip = "172.16.1.234"
+    }
+  }
+  k8s-worker-02 = {
+    cpu = {
+        cores = 2
+    }
+    memory = {
+        amount = 4096
+    }
+    networking = {
+      ip = "172.16.1.235"
+    }
+  }
+}
 }
 # https://stackoverflow.com/questions/62403030/terraform-wait-till-the-instance-is-reachable
 resource "null_resource" "waiting_instances_ready" {
   # waiting for newly created instances to be ready to run ansible
-  for_each = toset(module.k8s-master-nodes-provision.instances_ip)
+  for_each = toset(concat(module.k8s-master-nodes-provision.instances_ip,module.k8s-worker-nodes-provision.instances_ip))
   provisioner "remote-exec" {
     connection {
       host = each.key
@@ -69,12 +100,20 @@ resource "null_resource" "waiting_instances_ready" {
     inline = ["while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done"]
   }
 }
-resource "ansible_host" "host" {
+resource "ansible_host" "master-nodes" {
   depends_on = [ null_resource.waiting_instances_ready ]
   for_each = toset(module.k8s-master-nodes-provision.instances_ip)
   name = each.key
   groups = ["k8s-masters"]
 }
+
+resource "ansible_host" "worker-nodes" {
+  depends_on = [ null_resource.waiting_instances_ready]
+  for_each = toset(module.k8s-worker-nodes-provision.instances_ip)
+  name = each.key
+  groups = ["k8s-workers"]
+}
+
 resource "null_resource" "running-ansible" {
   depends_on = [ null_resource.waiting_instances_ready ]
     provisioner "local-exec" {
--- kubernetes/installation/automated/cluster-provision/terraform-nodes-provision/output.tf
+++ kubernetes/installation/automated/cluster-provision/terraform-nodes-provision/output.tf
@@ -1,3 +1,3 @@
 output "ips" {
-  value = module.k8s-master-nodes-provision.instances_ip
+  value = concat(module.k8s-master-nodes-provision.instances_ip, module.k8s-worker-nodes-provision.instances_ip)
 }
